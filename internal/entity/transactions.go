c944039cc8c03a7f5e1479cdf0244b0d7a00b524
Andres Romo <andres.roqa93@gmail.com>
2022-05-05 14:36:51 -0500

    stori-5 reading and processing the CSV file

--- internal/entity/transactions.go
+++ internal/entity/transactions.go
@@ -1,14 +1,9 @@
 package entity
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
-
 type (
-	DataFraud struct {
-		ID          string             `json:"id" bson:"_id"`
-		Date        primitive.DateTime `bson:"date" json:"date"`
-		Transaction string             `bson:"transaction" json:"transaction"`
-		CreatedAt   primitive.DateTime `bson:"created_at,omitempty" json:"created_at,omitempty"`
-		UpdatedAt   primitive.DateTime `bson:"updated_at,omitempty" json:"updated_at,omitempty"`
-		DeletedAt   primitive.DateTime `bson:"deleted_at,omitempty" json:"deleted_at,omitempty"`
+	Transaction struct {
+		ID          int    `json:"id" bson:"id"`
+		Date        string `bson:"date" json:"date"`
+		Transaction string `bson:"transaction" json:"transaction"`
 	}
 )
